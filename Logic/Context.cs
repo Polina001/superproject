061046add684337538a37114ef0006f1ca922737
Полина Кудрявцева <kudpolser@mail.ru>
2016-12-10 18:55:49 +0300

    I created a database! But it is empty :c

---
+++ Logic/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    class Context : DbContext
+    {
+        public DbSet<Theme> Themes { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Inscription> Inscriptions { get; set; }
+
+        public Context() : base("localsql")
+        {
+
+        }
+    }
+}
---
+++ Logic/Theme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    class Theme
+    {
+        public int ID { get; set; }
+        public string ThemeName { get; set; }
+    }
+}
