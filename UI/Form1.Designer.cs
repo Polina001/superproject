94dc9d8a70c9df8d5e503ff1095298274357b8fa
Наталья Полякова <napolyakova@edu.hse.ru>
2016-12-10 21:32:58 +0300

    New UI form!!! See it. I think it's more interesting than previous was. (надо дополнительно установить расширение NuGet Metroframework)

--- UI/Form1.Designer.cs
+++ UI/Form1.Designer.cs
@@ -28,47 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChrisPost));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(23, 73);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(306, 265);
+            this.pictureBox1.Size = new System.Drawing.Size(477, 384);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(373, 48);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = " ";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(41, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Здесь будет ваша картинка";
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(572, 73);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(114, 26);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Lime;
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
             // 
             // ChrisPost
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(509, 357);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(901, 480);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.Name = "ChrisPost";
-            this.Text = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Lime;
+            this.Text = "ChristCards";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroButton metroButton1;
     }
 }
 
--- UI/Form1.cs
+++ UI/Form1.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MetroFramework.Components;
+using MetroFramework.Forms;
 
 namespace UI
 {
-    public partial class ChrisPost : Form
+    public partial class ChrisPost : MetroForm
     {
         public ChrisPost()
         {
             InitializeComponent();
+            MetroStyleManager.Default.Style = MetroFramework.MetroColorStyle.Lime;
+            MetroStyleManager.Default.Theme = MetroFramework.MetroThemeStyle.Dark;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
