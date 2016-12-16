1873e65610dad4acd6d3eacc6b820595a879e02d
Мария Курепова <mkurepova@gmail.com>
2016-12-16 13:47:41 +0300

    я снова добавила метод вывода в пикчер бокс, помолючь, чтобы он добавился и у вас

---
+++ Logic/OutPuttingInPb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logic
+{
+    public class OutPuttingInPb
+    {
+        public void OutputtingPicture(PictureBox pb, System.Drawing.Image image)
+        {
+            pb.Location = new Point(37, 137);
+            pb.Image = image;
+
+        }
+    }
+}
